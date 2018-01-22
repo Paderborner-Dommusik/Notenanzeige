f0267424ffdf0b4f46b343fe4c91289260ef540a
firetailor <mail@firetailor.de>
2018-01-22 13:21:49 +0100

    Simple View done :)

--- DoPaNoZe_Main/MainWindow.xaml.cs
+++ DoPaNoZe_Main/MainWindow.xaml.cs
@@ -55,6 +55,13 @@ namespace NoZe_Main
 
         public bool IsNoteOn(int midiID)
         {
+            Image glowElement = null;
+            midiToView.TryGetValue(midiID, out glowElement);
+            if (glowElement != null)
+            {
+                return glowElement.Opacity == 0.0 ? false : true;
+            }
+
             return false;
         }
 
@@ -73,34 +80,35 @@ namespace NoZe_Main
 
         private void _Loaded(object sender, RoutedEventArgs e)
         {
-            midiToView.Add(55, note_1);
-            midiToView.Add(56, null);
-            midiToView.Add(57, note_2);
-            midiToView.Add(58, null);
-            midiToView.Add(59, note_3);
-            midiToView.Add(60, note_4);
-            midiToView.Add(61, null);
-            midiToView.Add(62, note_5);
-            midiToView.Add(63, null);
-            midiToView.Add(64, note_6);
-            midiToView.Add(65, note_7);
-            midiToView.Add(66, null);
-            midiToView.Add(67, note_8);
-            midiToView.Add(68, null);
-            midiToView.Add(69, note_9);
-            midiToView.Add(70, null);
-            midiToView.Add(71, note_10);
-            midiToView.Add(72, note_11);
-            midiToView.Add(73, null);
-            midiToView.Add(74, note_12);
-            midiToView.Add(75, null);
-            midiToView.Add(76, note_13);
-            midiToView.Add(77, note_14);
-            midiToView.Add(78, null);
-            midiToView.Add(79, note_15);
-            midiToView.Add(80, null);
-            midiToView.Add(81, note_16);
-            midiToView.Add(82, note_17);
+            midiToView.Add(57, note_1);
+            midiToView.Add(58, hnote_1);
+            midiToView.Add(59, note_2);
+            midiToView.Add(60, note_3);
+            midiToView.Add(61, hnote_2);
+            midiToView.Add(62, note_4);
+            midiToView.Add(63, hnote_3);
+            midiToView.Add(64, note_5);
+            midiToView.Add(65, note_6);
+            midiToView.Add(66, hnote_4);
+            midiToView.Add(67, note_7);
+            midiToView.Add(68, hnote_5);
+            midiToView.Add(69, note_8);
+            midiToView.Add(70, hnote_6);
+            midiToView.Add(71, note_9);
+            midiToView.Add(72, note_10);
+            midiToView.Add(73, hnote_7);
+            midiToView.Add(74, note_11);
+            midiToView.Add(75, hnote_8);
+            midiToView.Add(76, note_12);
+            midiToView.Add(77, note_13);
+            midiToView.Add(78, hnote_9);
+            midiToView.Add(79, note_14);
+            midiToView.Add(80, hnote_10);
+            midiToView.Add(81, note_15);
+            midiToView.Add(82, hnote_11);
+            midiToView.Add(83, note_16);
+            midiToView.Add(84, note_17);
+            midiToView.Add(85, hnote_12);
 
             foreach (int a in midiToView.Keys)
             {
